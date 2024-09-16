6108084771f75762f3df010cc972673d43818491
TriLLuxe <glazunovkd@gmail.com>
2024-09-16 21:13:06 +0700

    Подредачил чтобы было видно комменты

--- TEST2/Program.cs
+++ TEST2/Program.cs
@@ -97,58 +97,58 @@ class ProgramMain
             return $"{LastName} {FirstName} {Patronymic}; {sexFormatted}; {weightFormatted}; {dateOfBirthFormatted}; {Age}";
         }
     }
-// public class Person{
-// 	public string LastName { get; }
-// 		public string FirstName { get; }
-// 		public string Patronymic { get; }
-// 		private bool Sex;
-
-// 		public float Weight { get; }
-// 		public DateTime DateOfBirth { get; }
-// 		public int Age { get { return CalculateAge(); } }
-
-// 		public Person(string lastName, string firstName, string patronymic, bool sex, float weight, DateTime dateOfBirth)
-// 		{
-// 			LastName = lastName;
-// 			FirstName = firstName;
-// 			Patronymic = patronymic;
-// 			Sex = sex;
-// 			Weight = weight;
-// 			DateOfBirth = dateOfBirth;
-// 		}
-// 		 private int CalculateAge()
-//         {
-//             var today = DateTime.Today;
-//             var age = today.Year - DateOfBirth.Year;
-//             if (today.Month < DateOfBirth.Month || (today.Month == DateOfBirth.Month && today.Day < DateOfBirth.Day))
-//             {
-//                 age--;
-//             }
-//             return age;
-//         }
-// 		 public static Person Parse(string text)
-//         {
-//             var parts = text.Split(';');
-//             if (parts.Length != 6) throw new FormatException("Invalid format of input string");
-
-//             string lastName = parts[0];
-//             string firstName = parts[1];
-//             string patronymic = parts[2];
-//             bool sex = parts[3] == "Муж." ? true : parts[3] == "Жен." ? false : throw new FormatException("Invalid sex format");
-//             float weight = float.Parse(parts[4]);
-//             DateTime dateOfBirth = DateTime.Parse(parts[5]);
-// 			if (dateOfBirth>DateTime.Now) throw new FormatException("Invalid date of birth format {dateOfBirth}");
-
-//             return new Person(lastName, firstName, patronymic, sex, weight, dateOfBirth);
-//         }
-
-//         public override string ToString()
-//         {
-//             string dateOfBirthFormatted = DateOfBirth.ToString("dd-MM-yyyy");
-//             string weightFormatted = Weight.ToString("E1");
-//             string sexFormatted = Sex ? "Муж." : "Жен.";
-//             return $"{LastName} {FirstName} {Patronymic}; {sexFormatted}; {weightFormatted}; {dateOfBirthFormatted}; {Age}";
-//         }
-// }
-
 }
+/*public class Person{
+	public string LastName { get; }
+		public string FirstName { get; }
+		public string Patronymic { get; }
+		private bool Sex;
+
+		public float Weight { get; }
+		public DateTime DateOfBirth { get; }
+		public int Age { get { return CalculateAge(); } }
+}
+
+		public Person(string lastName, string firstName, string patronymic, bool sex, float weight, DateTime dateOfBirth)
+		{
+			LastName = lastName;
+			FirstName = firstName;
+			Patronymic = patronymic;
+			Sex = sex;
+			Weight = weight;
+			DateOfBirth = dateOfBirth;
+		}
+		 private int CalculateAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+            if (today.Month < DateOfBirth.Month || (today.Month == DateOfBirth.Month && today.Day < DateOfBirth.Day))
+            {
+                age--;
+            }
+            return age;
+        }
+		 public static Person Parse(string text)
+        {
+            var parts = text.Split(';');
+            if (parts.Length != 6) throw new FormatException("Invalid format of input string");
+
+            string lastName = parts[0];
+            string firstName = parts[1];
+            string patronymic = parts[2];
+            bool sex = parts[3] == "Муж." ? true : parts[3] == "Жен." ? false : throw new FormatException("Invalid sex format");
+            float weight = float.Parse(parts[4]);
+            DateTime dateOfBirth = DateTime.Parse(parts[5]);
+			if (dateOfBirth>DateTime.Now) throw new FormatException("Invalid date of birth format {dateOfBirth}");
+
+            return new Person(lastName, firstName, patronymic, sex, weight, dateOfBirth);
+        }
+
+        public override string ToString()
+        {
+            string dateOfBirthFormatted = DateOfBirth.ToString("dd-MM-yyyy");
+            string weightFormatted = Weight.ToString("E1");
+            string sexFormatted = Sex ? "Муж." : "Жен.";
+            return $"{LastName} {FirstName} {Patronymic}; {sexFormatted}; {weightFormatted}; {dateOfBirthFormatted}; {Age}";
+        }
+}*/
